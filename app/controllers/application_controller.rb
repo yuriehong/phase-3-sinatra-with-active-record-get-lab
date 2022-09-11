95645d1a3f43241c54b3e3a2f39abe08d3e83009
+ <+>
2022-09-11 15:01:06 -0400

    commit tree e84d5e67a61cf9818dd7d3881e33b62d8547d345

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,26 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
   # add routes
+    get '/bakeries' do
+      bakeries = Bakery.all
+      # send them back as a JSON array
+      bakeries.to_json
+    end
 
+    get '/bakeries/:id' do
+      # look up the bakery in the database using its ID
+      bakery = Bakery.find(params[:id])
+      bakery.to_json(include: :baked_goods)
+    end
+
+    get '/baked_goods/by_price' do
+      baked_goods = BakedGood.all.order(price: :desc)
+      baked_goods.to_json
+    end
+    
+    get '/baked_goods/most_expensive' do
+      baked_goods = BakedGood.all.order(price: :desc)[0]
+      baked_goods.to_json
+    end
 end
